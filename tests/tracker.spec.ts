9e3c7f763b6d3cf12ee0dda2c2dc4bdc593776f3
Fran <wei840220@gmail.com>
2024-01-21 14:32:20 +0800

    feat: enable the github workflows

--- tests/tracker.spec.ts
+++ tests/tracker.spec.ts
@@ -4,7 +4,7 @@ import path from 'path';
 function testForImageTracker (images: string[], expectedResult: { pull: string; name: string}[]) {
   return async () => {
     const browser = await chromium.launch({
-      headless: false
+      headless: true
     });
     const context = await browser.newContext();
     const page = await context.newPage();
